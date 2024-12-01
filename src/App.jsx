edd6c816c6d56ade2e53d1fb50b76233130a231d
Tariq Jamal A <tariqjamal4267324@gmail.com>
2024-12-01 20:49:15 +0530

    feat: configured tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,15 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <h1 className="font-bold text-3xl  underline text-purple-600 md:text-red-900 text-blue-900b sm:text-green-900">
+                Hello world!
+            </h1>
+        </>
+    )
 }
 
 export default App

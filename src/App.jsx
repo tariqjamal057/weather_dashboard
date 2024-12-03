7116b8bbdc2b5c0db2eab4be6bb1a9dab6b8ea03
Tariq Jamal A <tariqjamal4267324@gmail.com>
2024-12-03 21:53:49 +0530

    feat: add layout for app and update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Layout from './components/Layout'
+
 
 function App() {
     const [count, setCount] = useState(0)
 
     return (
-        <>
-            <h1 className="font-bold text-3xl  underline text-purple-600 md:text-red-900 text-blue-900b sm:text-green-900">
-                Hello world!
-            </h1>
-        </>
+        <BrowserRouter>
+            <Layout>
+                <Routes>
+                    <Route path='/' element={<h1>Home</h1>}></Route>
+                    <Route path='/saved' element={<h1>Saved </h1>}></Route>
+                </Routes>
+            </Layout>
+        </BrowserRouter>
     )
+            
 }
 
 export default App
---
+++ src/components/Layout.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Header from './Header'
+import Footer from './Footer';
+
+const Layout = ({children}) => {
+  return (
+    <div className='flex w-full'>
+        <Navbar/>
+        <div className='flex flex-col w-full p-4 bg-gray-50'>
+            <Header/>
+            {children}
+            <Footer/>
+        </div>
+    </div>
+  )
+}
+
+export default Layout

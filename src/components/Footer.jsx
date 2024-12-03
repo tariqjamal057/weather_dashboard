0e0ae55063493f40ff6189d15f7edcc8a3df0059
Tariq Jamal A <tariqjamal4267324@gmail.com>
2024-12-03 23:37:31 +0530

    feat: add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="text-center text-white rounded absolute w-[95%] bottom-0 bg-blue-700 py-2 my-2 mt-4">
+      <a href="https://github.com/tariqjamal057" target="_blank">
+        Made by Tariq Jamal
+      </a>
+    </div>
+  );
+};
+
+export default Footer;

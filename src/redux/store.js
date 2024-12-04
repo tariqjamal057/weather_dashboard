c4fb5d7aac2ebb9b26c1dd429655bf027688a084
Tariq Jamal A <tariqjamal4267324@gmail.com>
2024-12-05 00:08:37 +0530

    feat: initialised redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,12 @@
-
 import { configureStore } from '@reduxjs/toolkit';
 import sidebarReducer from './slices/sidebar';
+import citiesSlice from './slices/cities';
+import weatherReducer from './slices/weather/weather';
 
 export const store = configureStore({
   reducer: {
     sidebar: sidebarReducer,
+    cities: citiesSlice,
+    weather: weatherReducer,
   },
 });

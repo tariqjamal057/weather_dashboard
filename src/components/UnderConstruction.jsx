12ef486ee9d53066c89cfb174bc6b627b2918771
Tariq Jamal A <tariqjamal4267324@gmail.com>
2024-12-06 20:43:44 +0530

    refactor: update col-span style for page sepcific

--- src/components/UnderConstruction.jsx
+++ src/components/UnderConstruction.jsx
@@ -3,7 +3,7 @@ import { FaTools, FaClock, FaRegSadTear } from "react-icons/fa";
 
 const UnderConstruction = ({ botton_container }) => {
   return (
-    <div className={`min-h-42 ${botton_container ? 'lg:min-h-[calc((100vh-145px)/2.15)]' : 'min-h-[80vh]'} col-span-12 bg-white rounded-lg flex flex-col justify-center items-center text-center ${botton_container ? "order-3" : ""} p-4`}>
+    <div className={`min-h-42 ${botton_container ? 'lg:min-h-[calc((100vh-145px)/2.15)]' : 'min-h-[80vh]'}  bg-white rounded-lg flex flex-col justify-center items-center text-center ${botton_container ? "order-4 lg:col-span-9 col-span-12" : "col-span-12"} p-4`}>
       <h2 className="text-red-600 font-bold text-2xl mb-2 flex items-center">
         <FaTools className="mr-2" /> Under Construction
       </h2>

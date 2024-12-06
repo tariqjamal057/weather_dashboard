e0831c1f33986f0cafb9540914c99b5457888281
Tariq Jamal A <tariqjamal4267324@gmail.com>
2024-12-06 19:57:38 +0530

    refactor: add component to set the page title and added them in page's

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,8 +2,10 @@ import CurrentWeather from "../components/weather/CurrentWeather";
 import WeatherMap from "../components/weather/WeatherMap";
 import TopCities from "../components/weather/TopCities";
 import UnderConstruction from "../components/UnderConstruction";
+import useDocumentTitle from "../components/layouts/useDocumentTitle";
 
 const Dashboard = () => {
+  useDocumentTitle("Weather App - Dashboard")
   return (
     <div className="min-h-[calc(100vh-145px)] mt-3">
       <div className="grid grid-cols-12 gap-2">
--- src/pages/SavedLocation.jsx
+++ src/pages/SavedLocation.jsx
@@ -1,6 +1,8 @@
+import useDocumentTitle from "../components/layouts/useDocumentTitle";
 import UnderConstruction from "../components/UnderConstruction";
 
 const SavedLocation = () => {
+  useDocumentTitle("Weather App - Saved Locations")
   return (
     <>
       <UnderConstruction botton_container={false} />
